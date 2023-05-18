c5107648fa1a5f280da3a2042047ae6a5c0e6d28
lunathanael <68858103+lunathanael@users.noreply.github.com>
2023-05-18 00:04:37 -0400

    Update moves.h
    bool functions for movement, phone

--- Solitaire/moves.h
+++ Solitaire/moves.h
@@ -15,43 +15,43 @@ static inline void Update_pile(PILE* pile) {
 
 
 
-static inline void Turn_stock(STOCK* stock) {
+static inline bool Turn_stock(STOCK* stock) {
 	if (stock->cardsLeft == 0) {
 		// ILLEGAL MOVE
-		return;
+		return false;
 	}
 	while (true) {
 		++stock->faceCard;
 		if (stock->faceCard > STOCK_SIZE) {
 			stock->faceCard = -1;
-			return;
+			return true;
 		}
 		if (stock->faceCard == STOCK_SIZE) {
-			return;
+			return true;
 		}
 		if (stock->stack[stock->faceCard] != NO_CARD) {
-			return;
+			return true;
 		}
 	}
 };
 
-static inline void Stock_to_foundation(STOCK* stock, FOUNDATION* foundation, int foundationIndex) {
+static inline bool Stock_to_foundation(STOCK* stock, FOUNDATION* foundation, int foundationIndex) {
 	if (stock->faceCard != -1 && stock->faceCard != STOCK_SIZE) { // faceCard exists
 		CARDS stock_card = stock->stack[stock->faceCard];
 
 		// Check for same suit and 1 higher
 		if (get_card_suit(stock_card) != foundationIndex) {
 			// ILLEGAL MOVE, not same suit
-			return;
+			return false;
 		}
 		if (foundation->cards == 0) {
 			if (get_card_rank(stock_card) != ACE) {
-				return;
+				return false;
 			}
 		}
 		else if ((stock_card - 1) != (foundation->stack[foundation->cards - 1])) {
 			// ILLEGAL MOVE, not 1 higher
-			return;
+			return false;
 		}
 
 
@@ -62,27 +62,28 @@ static inline void Stock_to_foundation(STOCK* stock, FOUNDATION* foundation, int
 		stock->stack[stock->faceCard] = NO_CARD;
 		Turn_stock(stock);
 		--stock->cardsLeft;
+  return true;
 	}
-	return;
+	return false;
 }
 
-static inline void Pile_to_foundation(PILE* pile,  FOUNDATION* foundation, int foundationIndex) {
+static inline bool Pile_to_foundation(PILE* pile,  FOUNDATION* foundation, int foundationIndex) {
 	if (pile->cardNumber != 0) { // cards exist
 		CARDS pile_card = pile->pile[pile->cardNumber - 1]; // Top card
 
 		// Check for same suit and 1 higher
 		if (get_card_suit(pile_card) != foundationIndex) {
 			// ILLEGAL MOVE, not same suit
-			return;
+			return false;
 		}
 		if (foundation->cards == 0) {
 			if (get_card_rank(pile_card) != ACE) {
-				return;
+				return false;
 			}
 		}
 		else if ((pile_card - 1) != (foundation->stack[foundation->cards - 1])) {
 			// ILLEGAL MOVE, not 1 higher
-			return;
+			return false;
 		}
 
 		// add foundation card;
@@ -91,26 +92,27 @@ static inline void Pile_to_foundation(PILE* pile,  FOUNDATION* foundation, int f
 		// remove pile card
 		pile->pile[--pile->cardNumber] = NO_CARD;
 		Update_pile(pile);
+  return true;
 	}
-	return;
+	return false;
 }
 
-static inline void Foundation_to_pile(PILE* pile,  FOUNDATION* foundation, int foundationIndex) {
+static inline bool Foundation_to_pile(PILE* pile,  FOUNDATION* foundation, int foundationIndex) {
 	if (foundation->cards != 0) { // cards exist
 		CARDS foundation_card = foundation->stack[foundation->cards - 1]; // Top card
 
 		if (pile->cardNumber != 0) {
 			// Check for opposite color suit and one lower
 			if (get_card_rank(foundation_card) == KING) {
-				return;
+				return false;
 			}
 			else if ((foundationIndex >> 1) ^ (get_card_suit(pile->pile[pile->cardNumber - 1]) >> 1)) {
 				// ILLEGAL MOVE, not same suit
-				return;
+				return false;
 			}
 			else if ((foundation_card + 1) != (foundation->stack[foundation->cards - 1])) {
 				// ILLEGAL MOVE, not 1 higher
-				return;
+				return false;
 			}
 		}
 
@@ -120,8 +122,9 @@ static inline void Foundation_to_pile(PILE* pile,  FOUNDATION* foundation, int f
 
 		// remove foundation card;
 		foundation->stack[--foundation->cards] = NO_CARD;
+  return true;
 	}
-	return;
+	return false;
 }
 
 static inline void Pile_to_pile(PILE* pile, int card_index, FOUNDATION* foundation, int foundationIndex) {
